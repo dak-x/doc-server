131221f483820bd814477880ada4a885ad9d7ae0
Daksh <dak-x@outlook.com>
2020-12-30 23:01:24 +0530

    Simplified Crate-Name , Static Request Handling 	* Removed dependencies: toml , rocket_contrib 	* Added crate_name through env!(CARGO_OKG_NAME) 	* Removed StaticFiles! and used simple file-name match

--- src/main.rs
+++ src/main.rs
@@ -3,51 +3,39 @@
 extern crate rocket;
 
 use convert_case::*;
-use rocket::response::Redirect;
-use rocket_contrib::serve::StaticFiles;
-use std::fs::File;
-use std::path::Path;
+use rocket::response::{NamedFile, Redirect};
+use std::fs;
+use std::io;
+use std::os::linux::fs::MetadataExt;
+use std::path::{Path, PathBuf};
+
 
 #[get("/")]
 fn index() -> Redirect {
     let root = get_rootname().unwrap().to_case(Case::Snake);
     let path = format!("/{}/index.html", root);
-    
-    Redirect::to(path.to_owned())
+    Redirect::permanent(path.to_owned())
 }
+#[get("/<file..>")]
+fn static_files(file: PathBuf) -> io::Result<NamedFile> {
+    NamedFile::open(Path::new("./target/doc/").join(file))
+}
+
 
 fn main() {
     rocket::ignite()
-        .mount("/", StaticFiles::from("./target/doc"))
-        .mount("/", routes!(index))
+        // .mount("/", StaticFiles::from("./target/doc"))
+        .mount("/", routes!(index, static_files))
         .launch();
 }
 
 fn get_rootname() -> Result<String, String> {
-    let cargo_toml =
-        std::fs::read_to_string("Cargo.toml").expect("Cannot Find Cargo.toml in the workspace");
-    let cargo_toml: toml::Value = toml::from_str(cargo_toml.as_str()).unwrap();
-
-    let package: &toml::Value = cargo_toml
-        .get("package")
-        .ok_or("Cannot find header `package` in `Cargo.toml`")?;
-
-    package
-        .get("name")
-        .map(|name| {
-            let name = name.to_string();
-            name.trim_start_matches("\"")
-                .trim_end_matches("\"")
-                .to_string()
-        })
-        .ok_or(
-            "Cannot find `name` inside \
-                `package` in `Cargo.toml` "
-                .to_owned(),
-        )
+    let pkg_name = env!("CARGO_PKG_NAME");
+    Ok(pkg_name.to_string())
 }
 
 #[test]
 fn test() {
-    assert_eq!(get_rootname().unwrap(), "doc-server");
+    let pkg_name = env!("CARGO_PKG_NAME");
+    assert_eq!(pkg_name, "doc-server");
 }
